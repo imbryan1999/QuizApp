52102178f12f0c450e64354907102d726391e5d3
Bryan <hemantshrsth@gmail.com>
2021-11-16 01:17:01 +0530

    Sending Success(Score) through Intents

--- app/src/main/java/android/example/quizapp/GameWonFragment.kt
+++ app/src/main/java/android/example/quizapp/GameWonFragment.kt
@@ -1,30 +1,53 @@
 package android.example.quizapp
 
+import android.content.Intent
 import android.example.quizapp.databinding.FragmentGameWonBinding
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.view.*
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false
+        )
 
-        binding.nextMatchButton.setOnClickListener { view:View ->
-            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragment2ToGameFragment())
+        binding.nextMatchButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(GameWonFragmentDirections.actionGameWonFragment2ToGameFragment())
         }
 
-        val args = GameWonFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context,"NumCorrect: ${args.numCorrect}" +
-                ", NumQuestion: ${args.numQuestions}", Toast.LENGTH_LONG).show()
-
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .setType("plain/text").intent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/android/example/quizapp/TitleFragment.kt
+++ app/src/main/java/android/example/quizapp/TitleFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
@@ -14,8 +13,6 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        // return inflater.inflate(R.layout.fragment_title, container, false)
 
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
